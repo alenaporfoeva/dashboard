5cd632200ce01c4c15ca11192f47b04d90149655
Alena <alenaporfoevakorr@gmail.com>
2026-04-23 23:17:43 +0200

    feat: add sidebar and add project/employee buttons

---
+++ script.js
@@ -0,0 +1,75 @@
+// Side-panel
+const sidePanel = document.getElementById("side-panel");
+const toggleButton = document.getElementById("toggle-button");
+const openButton = document.getElementById("open-button");
+const mainContent = document.getElementById("main-content");
+
+toggleButton.addEventListener("click", () => {
+  sidePanel.classList.add("collapsed");
+  mainContent.classList.add("expanded");
+  openButton.classList.remove("hidden");
+});
+
+openButton.addEventListener("click", () => {
+  sidePanel.classList.remove("collapsed");
+  mainContent.classList.remove("expanded");
+  openButton.classList.add("hidden");
+});
+
+// Add project button
+const addProjectBtn = document.getElementById("add-project-btn");
+const projectPanel = document.getElementById("project-panel");
+const cancelProjectBtn = document.getElementById("cancel-project-btn");
+
+addProjectBtn.addEventListener("click", () => {
+  projectPanel.classList.add("open");
+});
+
+cancelProjectBtn.addEventListener("click", () => {
+  projectPanel.classList.remove("open");
+});
+
+// Add employee button
+const addEmployeeBtn = document.getElementById("add-employee-btn");
+const employeePanel = document.getElementById("employee-panel");
+const cancelEmployeeBtn = document.getElementById("cancel-employee-btn");
+
+addEmployeeBtn.addEventListener("click", () => {
+  employeePanel.classList.add("open");
+});
+
+cancelEmployeeBtn.addEventListener("click", () => {
+  employeePanel.classList.remove("open");
+});
+
+// Submit of project and employee form
+const projectForm = document.getElementById("project-form");
+const employeeForm = document.getElementById("employee-form");
+
+projectForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+employeeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+// Seed Data popup
+const seedBtn = document.getElementById("seed-data-btn");
+const seedModal = document.getElementById("seed-modal");
+const overlay = document.getElementById("seed-overlay");
+const closeSeedModal = document.getElementById("close-seed-modal");
+
+seedBtn.addEventListener("click", () => {
+  seedModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+closeSeedModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+function closeModal() {
+  seedModal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
